7f7ee187761407903aa20dcdf0b09e993e493a4b
saparker <stephanieparker1997@gmail.com>
2022-10-14 20:36:45 -0400

    added 6 of 8 arrays of images, still figuring out logic

---
+++ models/images.js
@@ -0,0 +1,27 @@
+const mongoose = require('./connection')
+
+const { Schema } = mongoose
+
+const comfyCozy = ["https://imgur.com/UhTQJ0a","https://imgur.com/gigVpzi", "https://imgur.com/fskqfXW"]
+
+const critters = ["https://imgur.com/wk1Yrzw","https://imgur.com/WhMZILv","https://imgur.com/DrJsPtN","https://imgur.com/mfCkfvf", "https://imgur.com/0QKejrF",]
+
+const water = ["https://imgur.com/2FjYELq", "https://imgur.com/dvrJDGc", "https://imgur.com/b87CLnu", "https://imgur.com/vOtlK6P", "https://imgur.com/GfvenPD", "https://imgur.com/vt8Al5V", "https://imgur.com/Gp2SiAS", "https://imgur.com/pGpMkJC"]
+
+const rocks = ["https://imgur.com/ifC2sMA", "https://imgur.com/9Mfgv77", "https://imgur.com/6xZujNz", "https://imgur.com/t1Pl7qV", "https://imgur.com/abcL0Qr", "https://imgur.com/d6eUyef", "https://imgur.com/REvaLTY", "https://imgur.com/p6jXTic", "https://imgur.com/cQKxgE0", "https://imgur.com/HaVHHOD"]
+
+const vegetation = ["https://imgur.com/Wd3wn7T", "https://imgur.com/DRwjP9q", "https://imgur.com/hXwcCV9", "https://imgur.com/ua47jku", "https://imgur.com/w4ommzT", "https://imgur.com/uVLF9r2", "https://imgur.com/jDSLFJX", "https://imgur.com/iTE2G7s", "https://imgur.com/3mYLH53", "https://imgur.com/8GMDqYa", "https://imgur.com/F5uG5pi", "https://imgur.com/EMTlx2E", "https://imgur.com/HD1sMMI", "https://imgur.com/1UFYGWn", "https://imgur.com/PzsPbGo"]
+
+const sky = ["https://imgur.com/GUyWxpP", "https://imgur.com/4Yz5xMD", "https://imgur.com/5ML4Ba2", "https://imgur.com/GBjoNXc", "https://imgur.com/QThOJeu", "https://imgur.com/H0eIy5L", "https://imgur.com/mzOyRAk", "https://imgur.com/oLZxKdQ"]
+
+const 
+
+
+const imagesSchema = new Schema({
+    
+
+
+
+})
+
+module.exports = imagesSchema
